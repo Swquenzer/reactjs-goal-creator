37ce4ac626a8eed3bc98653b488dba200c50e7d6
Stephen Quenzer <swquenzer@gmail.com>
2017-10-30 13:46:57 -0400

    Dynamically update stat counters

--- src/components/goals-page.jsx
+++ src/components/goals-page.jsx
@@ -21,7 +21,9 @@ export default class GoalsPage extends Component {
          goalFilters: {
             interestFilter: null,
             otherFilter: null
-         }
+         },
+         numVisibleGoals: 0,
+         numHiddenGoals: 0
       }
    }
 
@@ -29,7 +31,7 @@ export default class GoalsPage extends Component {
       //Restore goals from database
       let cachedGoalItems = JSON.parse(localStorage.getItem('goalList'));
       if (cachedGoalItems != null && cachedGoalItems.length) {
-         this.setState({ goalItems: cachedGoalItems });
+         this.setState({ goalItems: cachedGoalItems }, this.updateStatCounter);
       } else {
          //this.createNewGoal(); //Add first goal
       }
@@ -38,12 +40,11 @@ export default class GoalsPage extends Component {
    componentDidUpdate(prevProps, prevState) {
       //console.log(JSON.parse(localStorage.getItem('goalList')));
 
-      //If any goals have changed, save to the database (local storage)
       if (!_IsEqual(prevState.goalItems, this.state.goalItems)) {
-         localStorage.setItem('goalList', JSON.stringify(this.state.goalItems));
+        //If any goals have changed, save to the database (local storage)
+        localStorage.setItem('goalList', JSON.stringify(this.state.goalItems));
+        this.updateStatCounter();
       }
-
-      console.log(this.state.goalItems);
    }
 
    render() {
@@ -57,38 +58,27 @@ export default class GoalsPage extends Component {
             <div className="col-lg-8 primary-col">
               <div className="row">
                 {
-                  this.state.goalItems.filter((goalItem) => {
-                    let showGoal = true;
-
-                    //Filter by "Interest"
-                    if(this.state.goalFilters.interestFilter && this.state.goalFilters.interestFilter !== goalItem.goalInterest) {
-                      showGoal = false;
-                    }
-
-                    //TODO: Other Filters
-
-                    //Return
-                    return showGoal;
-                  })
-                  .filter(g => g.isActive === true)
+                  this.state.goalItems
+                  .filter(g => g.isActive === true && (this.state.goalFilters.interestFilter === null || g.goalInterest === this.state.goalFilters.interestFilter))
                   .map((goalItem, index) => {
-                    return (<GoalItem key={goalItem.id} goalItem={_CloneDeep(goalItem)} updateGoalItem={this.updateGoalItem.bind(this, index)} />);
+                    return (<GoalItem key={goalItem.id} goalItem={_CloneDeep(goalItem)} updateGoalItem={this.updateGoalItem.bind(this)} />);
                   })
                 }
               </div>
             </div>
             <div className="col-lg-4">
-              <ul className="list-group">
-                <li className="list-group-item d-inline-flex justify-content-between">Number of total goals <span className="badge badge-primary badge-pill">{this.state.goalItems.length}</span></li>
-                <li className="list-group-item d-inline-flex justify-content-between">Visible Goals: <span className="badge badge-success badge-pill">{this.state.goalItems.length}</span></li>
-                <li className="list-group-item d-inline-flex justify-content-between">Hidden Goals: <span className="badge badge-secondary badge-pill">{0}</span></li>
-              </ul>
-              <div className="card mt-3">
+              <div className="card">
               	<div className="card-header">
               		Filter by Goal Type
               	</div>
               	<div className="card-body">
               		<GoalFilters filters={this.state.goalFilters} setInterestFilter={filterType => this.setInterestFilter.call(this, filterType)} clearFilter={filterType => this.clearFilter.call(this, filterType)} />
+                  <ul className="list-group mt-3">
+                    <li className="list-group-item d-inline-flex justify-content-between">Number of total goals <span className="badge badge-primary badge-pill">{this.state.goalItems.length}</span></li>
+                    <li className="list-group-item d-inline-flex justify-content-between">Visible Goals: <span className="badge badge-success badge-pill">{this.state.numVisibleGoals}</span></li>
+                    <li className="list-group-item d-inline-flex justify-content-between">Hidden Goals: <span className="badge badge-secondary badge-pill">{this.state.numHiddenGoals}</span></li>
+                    <li className="list-group-item d-inline-flex justify-content-between">Deleted Goals: <span className="badge badge-danger badge-pill">{this.state.goalItems.filter(g => !g.isActive).length}</span></li>
+                  </ul>
               	</div>
               </div>
             </div>
@@ -100,10 +90,9 @@ export default class GoalsPage extends Component {
       );
    }
 
-   //list
+   //Goal Items
    createNewGoal() {
       let goalItems = [...this.state.goalItems]
-
       let newGoalItem = _CloneDeep(INITIAL_GOAL_ITEM);
       newGoalItem.id = this.state.goalItems.length + 1;
 
@@ -111,8 +100,7 @@ export default class GoalsPage extends Component {
       this.setState({ goalItems: goalItems })
    }
 
-   //list
-   updateGoalItem(index, goalItem) {
+   updateGoalItem(goalItem) {
       let goalItems = [...this.state.goalItems]
       const goalItemIndex = goalItems.findIndex(g => g.id === goalItem.id);
       
@@ -120,14 +108,22 @@ export default class GoalsPage extends Component {
       this.setState({ goalItems: goalItems })
    }
 
-   updateNumGoals(numGoals) {
-      this.setState({ numGoals });
+   //Stats
+   updateStatCounter() { //TODO: Move into filter control?
+      console.log("Updating Stat Counter")
+      const goalItems = [...this.state.goalItems];
+      const interestFilter = this.state.goalFilters.interestFilter;
+      this.setState({
+        numVisibleGoals: goalItems.filter(g => g.isActive && (interestFilter === null || g.goalInterest === interestFilter)).length,
+        numHiddenGoals: goalItems.filter(g => g.isActive && !(interestFilter === null || g.goalInterest === interestFilter)).length
+      });
    }
 
+   //Filters
    setInterestFilter(filter) {
       let goalFilters = _CloneDeep(this.state.goalFilters);
       goalFilters.interestFilter = filter;
-      this.setState({ goalFilters });
+      this.setState({ goalFilters }, this.updateStatCounter);
    }
 
    clearFilter(filterType) {
@@ -135,11 +131,15 @@ export default class GoalsPage extends Component {
       switch (filterType) {
          case "Interest":
             goalFilters.interestFilter = null;
-            this.setState({ goalFilters });
+            this.setState({ goalFilters }, this.updateStatCounter);
             break;
       }
    }
 
+   //Utilities
+
+
+   //Local Storage
    clearCache() {
      console.log("Clearing Cache");
      localStorage.setItem('goalList', JSON.stringify([]));
