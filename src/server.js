9b01f35382bc4cabc67c3dba646fb19de4167552
Stephen Quenzer <swquenzer@gmail.com>
2017-11-03 16:07:35 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
 // src/server.js
- 
+import Express from "express";
+var app = Express();
+
+app.use('/', express.static(__dirname))
+app.listen(45649, function() {
+	console.log("Listening");
+})
+
+ /*
 import path from "path";
 import {createExpressServer} from "create-react-server";
-import app from "./index";
+import app from "./app";
  
+console.log("In SErver.js");
+
 createExpressServer({
     port: process.env.PORT || 45649,
     app: app,
@@ -13,3 +23,4 @@ createExpressServer({
             `<div id="root">${html}</div>`)),
     outputPath: path.join(process.cwd(), 'build')
 });
+*/
